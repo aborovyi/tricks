5bfe90bdf22094f754f0a4058c2f600dfb0de22a
Andrii Borovyi <aborovyi@llnw.com>
2023-02-01 16:31:39 +0200

    Add script for screensharing in Slack and Zoom

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 Tricks
 ======
 
-This repository contains some small scripts (usually one-file) that I use daily, or want to chare with people, with hope they will be usefull.
-If you liked some of the present staff, feel free to fork it and improve.
-And yes, this is my first repository, so I'm studying how to manage it properly.
-
-:coffee:
+This repository contains some small and (sometimes) useful scripts to make my
+life easier with the daily routines.
---
+++ enable-screenshare.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script enables Screen sharing for Zoom and Slack, I depend on.
+
+# Tweak ~/.config/autostart/slack.desktop to enable sharing in slack
+SLACK_AUTOSTART_FILE="${HOME}/.config/autostart/slack.desktop"
+OS_RELEASE_FILE="/etc/os-release"
+
+echo -n "Checking Slack: "
+if test -f "$SLACK_AUTOSTART_FILE"; then
+    if [[ $(grep -E -q "/bin/slack %U$" "$SLACK_AUTOSTART_FILE") == 0 ]]; then
+        echo "updated."
+        sed -i \
+            's/\/bin\/slack %U/\/bin\/slack %U --enable-features=WebRTCPipeWireCapturer/' \
+            "$SLACK_AUTOSTART_FILE"
+    else
+        echo "no need to update."
+    fi
+else
+    echo "Slack not in autostart. Skipping."
+fi
+
+# Tweak /etc/os-release to enable desktop sharing in Zoom
+echo -e "Checking /etc/os-release to enable screen sharing in Zoom."
+VERSION_ID=$(grep VERSION_ID $OS_RELEASE_FILE)
+if [[ $VERSION_ID == "" ]]; then
+    echo "No VERSION_ID. Adding new one."
+    echo "VERSION_ID=999" >> "$SLACK_AUTOSTART_FILE"
+else 
+    if [[ $(echo "$VERSION_ID" | cut -d= -f2) != "999" ]]; then
+        echo "VERSION_ID exists. Updating."
+    else
+        echo "VERSION_ID is OK."
+    fi
+fi
